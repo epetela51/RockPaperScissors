dcf26150c6213eb28d43660d6400b2b04192d716
Eric Petela <epetela51@gmail.com>
2022-02-27 15:28:40 -0500

    Completed single round game logic Removed all functions and variables relating to assigning player or computer choice to value
    cleaned up and removed extra console.log calls

--- Javascript.js
+++ Javascript.js
@@ -11,64 +11,44 @@ function computerPlay() {
 //Variable that holds computers choice
 let computerChoice = computerPlay();
 
-//function that assigns numeric values to computer choice
-function assignComputerValue(choice) {
-    if (choice == "rock") {
-        return 3;
-    } else if (choice == "scissors") {
-        return 2
-    } else if (choice == "paper") {
-        return 1;
-    } else {
-        return "Something went wrong with computer choice";
-    }
-}
-
-//variable that holds the value for the computer choice
-let computerChoiceValue = assignComputerValue(computerChoice);
-
-console.log(`Computer chose ${computerChoice} which has a value assigned of ` + assignComputerValue(computerChoice));
-
-
-
-
-
 //variable that holds a players input
 let playerInput = prompt("Please choose either: Rock, paper or scissors");
+
 //variable that converts players choice to lowercase for easier comparison with computer choice
-let playerChoiceLowercase = playerInput.toLowerCase();
-
-//function that assigns numeric values to user input
-function assignUserInputValue(input) {
-    if (input == "rock") {
-        return 3;
-    } else if (input == "scissors") {
-        return 2
-    } else if (input == "paper") {
-        return 1;
-    } else {
-        return "Please choose 'Rock, Paper or Scissors"
-    }
-}
-
-//variable that holds the value for the players choice
-let playerChoiceValue = assignUserInputValue(playerChoiceLowercase);
-
-console.log(`Player chose ${playerChoiceLowercase} which has a value assigned of ` + assignUserInputValue(playerChoiceLowercase));
+let playerChoice = playerInput.toLowerCase();
 
 
-
-
-
-
-
-
-
-//function that plays a single round of Rock Paper Scisssors which takes two parameters - 1 from player and 1 from computer
+//function that takes a players choice and compares it to a computers choice to determine a winner
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection > computerSelection) {
-        return "You Win";
+    if (playerSelection == "rock") {
+        if (computerSelection == "scissors") {
+            return `You Win!  ${playerSelection} beats ${computerSelection}`;
+        } else if (computerSelection == "paper") {
+            return `You Lose!  ${computerSelection} beats ${playerSelection}`;
+        } else if (computerSelection == playerSelection) {
+            return "Tie, try again!"
+        }
+    } else if (playerSelection == "scissors") {
+        if (computerSelection == "paper") {
+            return `You Win!  ${playerSelection} beats ${computerSelection}`;
+        } else if (computerSelection == "rock") {
+            return `You Lose!  ${computerSelection} beats ${playerSelection}`;
+        } else if (computerSelection == playerSelection) {
+            return "Tie, try again!"
+        }
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "rock") {
+            return `You Win!  ${playerSelection} beats ${computerSelection}`;
+        } else if (computerSelection == "scissors") {
+            return `You Lose!  ${computerSelection} beats ${playerSelection}`;
+        } else if (computerSelection == playerSelection) {
+            return "Tie, try again!"
+        }
     } else {
-        return "You Lose";
+        return "Please enter either 'Rock, paper or scissors'";
     }
 }
+
+console.log(`Player chose ${playerChoice} and computer chose ${computerChoice}`)
+
+console.log(playRound(playerChoice, computerChoice))
