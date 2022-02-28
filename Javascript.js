3fb8b180d48a770d0271b514a0ce8eb40a6e3fb0
Eric Petela <epetela51@gmail.com>
2022-02-27 22:43:55 -0500

    Refactored Removed unecessary console.log and combined all into one using template literals
    Moved player input prompt into game function so player can make a new choice each round

--- Javascript.js
+++ Javascript.js
@@ -11,11 +11,6 @@ function computerPlay() {
 //Variable that holds computers choice
 let computerChoice = computerPlay();
 
-//variable that holds a players input
-let playerInput = prompt("Please choose either: Rock, paper or scissors");
-
-//variable that converts players choice to lowercase for easier comparison with computer choice
-let playerChoice = playerInput.toLowerCase();
 
 
 //function that takes a players choice and compares it to a computers choice to determine a winner
@@ -49,22 +44,27 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-
-
 let playerScore = 0;
 let computerScore = 0;
+
 
 //Function that plays multiple rounds
 function game() {
     // use a Do While loop so that the game can be played until either the player or computer hit 5 wins first
     do {
+        //variable that holds a players input
+        let playerInput = prompt("Please choose either: Rock, paper or scissors");
+
+        //variable that converts players choice to lowercase for easier comparison with computer choice
+        let playerChoice = playerInput.toLowerCase();
+        
         //this allows a new random choice to be made for the computer on each loop
         let computerTurn = computerPlay()
 
         //store the winner of the individual round
         let roundResult = playRound(playerChoice, computerTurn);
 
-        console.log(roundResult);
+        //console.log(roundResult);
 
         //Adds to the players score depending on who wins
         if (roundResult.search("Win") > 0) {
@@ -73,13 +73,13 @@ function game() {
             computerScore++;
         }
 
-        console.log(`Player Score is ${playerScore} and Computer Score is ${computerScore}`)
+        console.log(`${roundResult} | Player Score is ${playerScore} and Computer Score is ${computerScore}`)
 
         //Puts the final message up to declare a winner
         if (playerScore == 5) {
-            return console.log("CONGRATS! You are the winner")
+            return console.log("CONGRATS! You are the winner!")
         } else if (computerScore == 5) {
-            return console.log("OOPS! Computer is the winner. Try again LOSER")
+            return console.log("OOPS! Computer is the winner try again LOSER")
         } 
     } while (playerScore <= 5 || computerScore <= 5);
 }
