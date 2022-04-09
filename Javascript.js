259ecaaa37a78276ea0be4829e9fb8b04ce0af86
Eric Petela <epetela51@gmail.com>
2022-04-09 07:16:40 -0400

    Add HTML divs & JS reset game function
    Added divs for Game Over & to insert button
    Added function to show button when game ends and on click reset the game to play again

--- Javascript.js
+++ Javascript.js
@@ -72,9 +72,11 @@ function game(e) {
     //Puts the final message up to declare a winner
     if (playerScore == 5) {
         stopGame();
+        gameOver.textContent = "GAME OVER"
         return gameWinner.textContent = "CONGRATS! You are the winner!"
     } else if (computerScore == 5) {
         stopGame();
+        gameOver.textContent = "GAME OVER"
         return gameWinner.textContent = "OOPS! Computer is the winner try again LOSER"
     } 
 }
@@ -83,6 +85,32 @@ function stopGame() {
     playerRock.removeEventListener('click', game);
     playerPaper.removeEventListener('click', game);
     playerScissors.removeEventListener('click', game);
+
+    resetButton.appendChild(resetBtn)
+
+    resetButton.addEventListener('click', resetGame)
+}
+
+function resetGame() {
+    
+    //resets score for game and on screen
+    playerScore = 0;
+    playerCurrentScore.textContent = playerScore;
+    computerScore = 0;
+    computerCurrentScore.textContent = computerScore;
+
+    //resets choices, round & game winner display
+    displayPlayerChoice.textContent = '';
+    displayComputerChoice.textContent = '';
+    roundWinner.textContent = '';
+    gameWinner.textContent = '';
+    gameOver.textContent = '';
+    
+    resetBtn.remove();
+
+    playerRock.addEventListener('click', game)
+    playerPaper.addEventListener('click', game)
+    playerScissors.addEventListener('click', game)
 }
 
 
@@ -91,17 +119,22 @@ let playerRock = document.querySelector('#rock');
 let playerPaper = document.querySelector('#paper');
 let playerScissors = document.querySelector('#scissors');
 
+let resetBtn = document.createElement('button')
+resetBtn.innerText = 'Play Again';
+
 //display output
-let displayPlayerChoice = document.querySelector('#playerChoice')
-let displayComputerChoice = document.querySelector('#computerChoice')
-let roundWinner = document.querySelector('#roundWinner')
+let displayPlayerChoice = document.querySelector('#playerChoice');
+let displayComputerChoice = document.querySelector('#computerChoice');
+let roundWinner = document.querySelector('#roundWinner');
 let playerCurrentScore = document.querySelector('#playerScore');
 let computerCurrentScore = document.querySelector('#computerScore');
-let gameWinner = document.querySelector('#gameWinner')
+let gameWinner = document.querySelector('#gameWinner');
+let gameOver = document.querySelector('#gameOver');
+let resetButton = document.querySelector('#resetBtn');
 
 //on page load show 0 score at game start
-playerCurrentScore.textContent = 0;
-computerCurrentScore.textContent = 0;
+playerCurrentScore.textContent = playerScore;
+computerCurrentScore.textContent = computerScore;
 
 
 //on button click, get button text & display player vs. computer
